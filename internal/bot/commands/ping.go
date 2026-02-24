795743b55ad7687594694696046b08352ed7bc61
Akhror <axrorbekbebitovich97@gmail.com>
2026-02-24 14:10:23 +0500

    fix: help command

--- internal/bot/commands/ping.go
+++
@@ -1,8 +0,0 @@
-package commands
-
-import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-
-func Ping(api *tgbotapi.BotAPI, m *tgbotapi.Message) error {
-	_, err := api.Send(tgbotapi.NewMessage(m.Chat.ID, "pong 🏓"))
-	return err
-}
